9398286ea92f072e8452a3103b17f1caff75a769
Hein12345 <heinzawphyoe2@gmail.com>
2025-04-10 22:43:27 +0700

    Web route and controller and all blade

---
+++ resources/views/admin/sub_category/create.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.layouts.layout')
+@section('admin_page_title')
+    Create Sub Category - Admin Panel
+@endsection
+@section('admin_layout')
+    <h1>Create Sub Category Page</h1>
+@endsection
